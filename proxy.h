d4764b7b4ef937e3d9a2947730cf13d741ba5479
MaxDLink <max.durham.link@gmail.com>
2023-04-27 20:12:20 -0500

    modified to write to output file

--- proxy.h
+++ proxy.h
@@ -1,9 +1,18 @@
 #ifndef PROXY_H
 #define PROXY_H
 
-void handle_connections(int server_fd);
-int connect_to_target();
-void forward_request(int client_fd, int target_fd);
-void forward_response(int client_fd, int target_fd);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
-#endif
+int connect_to_target(FILE *output_file);
+void forward_request(int client_fd, int target_fd, FILE *output_file);
+void forward_response(int client_fd, int target_fd, FILE *output_file);
+void handle_connections(int server_fd, FILE *output_file);
+
+#endif /* PROXY_H */
+
