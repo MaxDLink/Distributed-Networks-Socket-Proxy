a44e2fc98a16b2c2d207242bf549b0e2a68c76e8
MaxDLink <max.durham.link@gmail.com>
2023-04-27 20:12:32 -0500

    modified to write to output file

--- server.c
+++ server.c
@@ -8,45 +8,42 @@
 
 #define PORT 8080
 
-int main(int argc, char const *argv[]) {
-
-    int server_fd, new_socket, valread;
+int main() {
+    int server_fd;
     struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[1024] = {0};
 
-    // Create socket
+    // Create socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
-
-    // Set socket options
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
-        perror("setsockopt failed");
         exit(EXIT_FAILURE);
     }
 
     // Bind socket to port 8080
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    address.sin_port = htons(8080);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
 
-    // Listen for incoming connections
     if (listen(server_fd, 3) < 0) {
         perror("listen failed");
         exit(EXIT_FAILURE);
     }
 
-    printf("Listening on port %d...\n", PORT);
+    // Declare file pointer
+    FILE *output_file;
+    output_file = fopen("output.txt", "w");
 
     // Handle incoming connections
-    handle_connections(server_fd);
+    handle_connections(server_fd, output_file);
+
+    // Close socket and output file
+    close(server_fd);
+    fclose(output_file);
 
     return 0;
 }
+
